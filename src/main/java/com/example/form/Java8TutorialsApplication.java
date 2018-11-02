7061fe55886e03504e442b294a51da12ab877d37
Jordi Artal <jordi.artal.castinfo@gmail.com>
2018-11-02 14:10:22 +0100

    empty spring project

---
+++ src/main/java/com/example/form/Java8TutorialsApplication.java
@@ -0,0 +1,12 @@
+package com.example.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8TutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8TutorialsApplication.class, args);
+	}
+}
