4f1bbfd8edfd317cdf040885b40ea53e4d9281a2
Jordi Artal <jordi.artal.castinfo@gmail.com>
2018-11-13 12:58:38 +0100

    example 3 finnished

--- src/main/java/com/mkyong/java8/example3/StreamFilter.java
+++ src/main/java/com/mkyong/java8/example3/StreamFilter.java
@@ -1,33 +1,25 @@
-/**
- * 
- */
 package com.mkyong.java8.example3;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-/**
- * @author Jordi.Artal
- *
- */
 public class StreamFilter {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
-		List<String> lines = Arrays.asList("spring", "node", "mkyong");
+		List<Person> persons = Arrays.asList(new Person("mkyong", 30), new Person("jack", 20),
+				new Person("lawrence", 40));
 
-		System.out.println("This method filters a List using Stream methods");
-		
-		List<String> result = lines.stream() // convert list to stream
-				.filter(line -> !"mkyong".equals(line)) // we dont like mkyong
-				.collect(Collectors.toList()); // collect the output and convert streams to a List
+		Person result1 = persons.stream() // Convert to steam
+				.filter(x -> "jack".equals(x.getName())) // we want "jack" only
+				.findAny() // If 'findAny' then return found
+				.orElse(null); // If not found, return null
 
-		result.forEach(System.out::println); // output : spring, node
+		System.out.println(result1);
+
+		Person result2 = persons.stream().filter(x -> "ahmook".equals(x.getName())).findAny().orElse(null);
+
+		System.out.println(result2);
 
 	}
-
 }
