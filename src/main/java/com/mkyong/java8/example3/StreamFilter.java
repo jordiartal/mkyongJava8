2287180513cd6e15359e71870d24ff85304bb25e
Jordi Artal <jordi.artal.castinfo@gmail.com>
2018-11-13 12:34:58 +0100

    example 3

--- src/main/java/com/mkyong/java8/example3/StreamFilter.java
+++ src/main/java/com/mkyong/java8/example3/StreamFilter.java
@@ -3,6 +3,10 @@
  */
 package com.mkyong.java8.example3;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Jordi.Artal
  *
@@ -13,7 +17,16 @@ public class StreamFilter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<String> lines = Arrays.asList("spring", "node", "mkyong");
+
+		System.out.println("This method filters a List using Stream methods");
+		
+		List<String> result = lines.stream() // convert list to stream
+				.filter(line -> !"mkyong".equals(line)) // we dont like mkyong
+				.collect(Collectors.toList()); // collect the output and convert streams to a List
+
+		result.forEach(System.out::println); // output : spring, node
 
 	}
 
